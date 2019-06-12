8dc9da5d9b280d5ace9bc3f639bf69723bcc06b9
claireyy23 <fancy51116@gmail.com>
2019-06-13 00:03:42 +0800

    map

--- main.cc
+++ main.cc
@@ -30,34 +30,46 @@ int main(int argc, char** argv){
     degree= new int[node_name.size()];
     cout<<"node_name size= "<<node_name.size()<<endl;
 
+//-----------------------count degree---------------------//
     for(int i=0;i<node_name.size();i++)
         degree[i]=0;
-
     for(int i=node_name.size()-1;i>0;i--){
-   cout<<" i= "<<i<<endl;
         for(int j=0;j<i;j++){
-   cout<<" j= "<<j<<endl;
-   //cout<<" node_name[i]= "<<node_name[i]->node_name<<endl;
             if(nm->connected(node_name[i]->name,node_name[j]->name)==0){
                 degree[i]++;
                 degree[j]++;
-
             }
         }
         if(degree[i]%2==1)
             odd_node.push_back(node_name[i]); 
-
     }
 
 //cout degree and odd nodes
 
     for(int x=0;x<node_name.size();x++)
         cout<<"degree ["<<node_name[x]->name<<"] = "<<degree[x]<<endl; 
+    cout<<"odd nodes = "<<odd_node.size()<<endl;
    
-      
-    
+    vector<vector<int>> map(node_name.size(),vector<int>(node_name.size(),0));
+    for(int i=0;i<node_name.size();i++)     
+        for(int j=0;j<node_name.size();j++) 
+            map[i][j]=0;
 
+    for(int i=node_name.size()-1;i>0;i--){
+        for(int j=0;j<i;j++){
+            if(nm->connected(node_name[i]->name,node_name[j]->name)==0){
+                map[i][j]++;
+                map[j][i]++;
+            }
+        }
+    }
 
+    for(int i=0;i<node_name.size();i++){     
+        for(int j=0;j<node_name.size();j++) {
+            cout<<"map ["<<i<<"] ["<<j<<"]= "<<map[i][j]<<"  ";
+            if(j==node_name.size()-1) cout<<endl;
+        }
+    }
 
 
 
